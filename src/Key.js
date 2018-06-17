9c4b48b7aa6379afb0b57ac53020d05c6b2f20e9
Lokesh Dhakar <lokesh.dhakar@gmail.com>
2018-06-16 21:22:10 -0700

    Add main loop method: update. Move kbd input into key.js

---
+++ src/Key.js
@@ -0,0 +1,63 @@
+/**
+ * Keyboard Input handler
+ *
+ * Example:
+ *
+ * const key = new Key();
+ * key.on();
+ *
+ * if (key.isDown('left')) { ... };
+ */
+class Key {
+    constructor() {
+      this.downKeys = {
+        up: false, // Includes spacebar
+        down: false,
+        left: false,
+        right: false,
+      }
+
+      this.onKeyUpOrDownBound = this.onKeyUpOrDown.bind(this);
+    }
+
+    on() {
+      window.addEventListener('keydown', this.onKeyUpOrDownBound);
+      window.addEventListener('keyup', this.onKeyUpOrDownBound);
+    }
+
+    off() {
+      window.removeEventListener('keydown', this.onKeyUpOrDownBound);
+      window.removeEventListener('keyup', this.onKeyUpOrDownBound);
+    }
+
+    onKeyUpOrDown(e) {
+      let toggleKey = (e.type === 'keydown');
+
+      switch (e.keyCode) {
+        case 38: // up
+        case 87: // w
+        case 32: // space bar
+          this.downKeys.up = toggleKey;
+          break;
+        case 40: // dpwn
+        case 83: // s
+          this.downKeys.down = toggleKey;
+          break;
+        case 37: // left
+        case 65: // a
+          this.downKeys.left = toggleKey;
+          break;
+        case 39: // right
+        case 68: // d
+          this.downKeys.right = toggleKey;
+          break;
+      }
+    }
+
+    isDown(key) {
+      return this.downKeys[key];
+    }
+}
+
+
+export default Key;
--- src/index.js
+++ src/index.js
@@ -1,5 +1,57 @@
+import c from './constants';
+import levels from './level-data';
+
+import Key from './Key';
+import Level from './Level';
 import Player from './Player';
 import World from './World';
 
 const world = new World();
 const player = new Player();
+const level1 = new Level(levels[0]);
+
+const canvas = document.getElementById('canvas');
+canvas.width = c.MAP_TW * c.GRID_SIZE;
+canvas.height = c.MAP_TH * c.GRID_SIZE;
+const ctx = canvas.getContext('2d');
+
+const key = new Key();
+key.on();
+
+
+const update = function() {
+  // ----------
+  // USER INPUT
+  // ----------
+
+  if (key.isDown('left')) {
+    player.left();
+  }
+
+  if (key.isDown('right')) {
+    player.right();
+  }
+
+  if (key.isDown('up')) {
+    player.up();
+  }
+
+  // ------
+  // UPDATE
+  // ------
+
+  player.update();
+
+  // ----
+  // DRAW
+  // ----
+
+  // Clear canvas
+  ctx.clearRect(0, 0, canvas.width, canvas.height)
+
+  // Draw Player
+  ctx.fillRect(player.x, player.y, player.width, player.height);
+  window.requestAnimationFrame(update);
+}
+
+update();
