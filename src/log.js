40eac21818378e9617195de02544afa622ba2d49
Lokesh Dhakar <lokesh.dhakar@gmail.com>
2018-06-17 14:06:21 -0700

    Add logged to help

---
+++ src/log.js
@@ -0,0 +1,27 @@
+/**
+ * Log global
+ */
+
+import c from './constants';
+
+class Log {
+  constructor() {
+    this.el = document.createElement('section');
+    this.el.setAttribute('id', 'log')
+    document.body.appendChild(this.el);
+  }
+  track(name, val) {
+    let trackEl = document.getElementById(`track-${name}`);
+    if (!trackEl) {
+      trackEl = document.createElement('div');
+      trackEl.setAttribute('id', `track-${name}`);
+      this.el.appendChild(trackEl);
+    }
+    let valString = JSON.stringify(val, null, 4);
+    trackEl.innerHTML = `${name}: ${valString}`;
+  }
+}
+
+const log = new Log();
+
+export default log;
