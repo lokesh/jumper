3668dd462967648b155f354770efa79bd3b10f3e
Lokesh Dhakar <lokesh.dhakar@gmail.com>
2018-06-17 11:21:09 -0700

    Mario tilesets to play with

---
+++ src/tiles/Mario.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset name="Mario" tilewidth="16" tileheight="16" spacing="1" margin="1" tilecount="2850" columns="75">
+ <image source="mario-tileset.png" trans="a2d8ff" width="1283" height="654"/>
+</tileset>
